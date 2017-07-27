e8aff107f04c404a6ebf4cdd3b8cbd3a061ae489
bruno ziiê <eu@brunoziie.com>
2017-07-27 13:38:59 -0300

    fix factories loader

--- src/Commands/Seed.js
+++ src/Commands/Seed.js
@@ -49,7 +49,7 @@ class Seed extends Command {
       const seedsPath = this.helpers.seedsPath()
       const selectedFiles = flags.files ? flags.files.split(',') : null
       const seedsFiles = this.loadSeedFiles(selectedFiles)
-      require(this.helpers.databasePath('factory'))
+      this.loadFactoriesFiles()
 
       yield this.seeder.exec(seedsFiles)
       this.success(`${this.icon('success')} seeded database successfully`)
